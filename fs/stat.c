928a978370971ff55bfc77950e53f868019333f8
zacky46 <zackyma777@gmail.com>
2023-05-21 15:06:06 +0000

    Revert "fs: test fix"
    This reverts commit b272cfbf41442999849832ce0034f6e00d6568d8.

--- fs/stat.c
+++ fs/stat.c
@@ -97,11 +97,11 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
 
-        ksu_handle_stat(&dfd, &filename, &flags);
-
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
 		goto out;
+
+	ksu_handle_stat(&dfd, &filename, &flags);
 
 	if (!(flag & AT_SYMLINK_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
