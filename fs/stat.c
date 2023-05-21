f04d830ee8507a44b12280e78fa0ec57d3faaa1f
zacky46 <zackyma777@gmail.com>
2023-05-21 11:52:47 +0000

    fs: test fix ../fs/stat.c:91:12: error: expected ';' after top level declarator                 int flag)                          ^                          ; ../fs/stat.c:95:1: error: expected identifier or '(' { ^ 2 errors generated.
    Change-Id: Ic7443a922f906b697e309c8c76fdd71bca0ebfeb

--- fs/stat.c
+++ fs/stat.c
@@ -87,7 +87,6 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
-// KernelSU hook
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user);
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
@@ -97,7 +96,8 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
 
-    ksu_handle_stat(&dfd, &filename); // call KSU hook first
+        ksu_handle_stat(&dfd, &filename);
+
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
 		goto out;
